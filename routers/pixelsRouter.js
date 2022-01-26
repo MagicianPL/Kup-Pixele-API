27a8deab8c1cfd101b0354e49b9e06d26bcddc71
Michał <sukowskimichal@gmail.com>
2022-01-27 00:00:30 +0100

    pixelRouter in build

--- routers/pixelsRouter.js
+++ routers/pixelsRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { trusted } = require("mongoose");
 const Pixel = require("../models/pixelModel");
 
 const pixelRouter = express.Router();
@@ -27,6 +28,36 @@ pixelRouter.get("/", async (req, res) => {
   try {
     const data = await Pixel.find();
     res.status(200).json(data);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+pixelRouter.get("/nonlimited", async (req, res) => {
+  try {
+    const data = await Pixel.find({ isLimited: false });
+    if (data.length > 0) {
+      res.status(200).json(data);
+      console.log(data.length);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Sorry, no more empty non limited pixels" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+pixelRouter.get("/limited", async (req, res) => {
+  try {
+    const data = await Pixel.find({ isLimited: true });
+    if (data.length > 0) {
+      res.status(200).json(data);
+      console.log(data.length);
+    } else {
+      res.status(404).json({ message: "Sorry, no more empty limited pixels" });
+    }
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
